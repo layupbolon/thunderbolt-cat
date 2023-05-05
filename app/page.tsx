8d6b4189bfc5103817ceadbdb4ff59f7b5b341c4
尹骁杰 <yinxiaojie@xforceplus.com>
2023-05-05 19:16:37 +0800

    feat: update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 import { Analytics } from '@vercel/analytics/react';
 import { Landing } from './components/landing';
+import Script from 'next/script';
 
 export default function App() {
   return (
     <>
       <Landing />
       <Analytics />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-1MKXZ1YR7T"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+
+           gtag('config', 'G-1MKXZ1YR7T');
+        `}
+      </Script>
     </>
   );
 }
